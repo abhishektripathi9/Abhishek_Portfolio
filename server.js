259300eced8849af46cfc5e155274387d9e4ea16
Abhishek Tripathi <at180887@gmail.com>
2026-09-25 23:09:23 +0530

    feat: optimize Google Search and Image SEO, add Person schema JSON-LD, and image sitemap

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ const MIME_TYPES = {
   '.svg': 'image/svg+xml',
   '.ico': 'image/x-icon',
   '.pdf': 'application/pdf',
+  '.xml': 'application/xml; charset=utf-8',
+  '.txt': 'text/plain; charset=utf-8',
   '.woff': 'font/woff',
   '.woff2': 'font/woff2',
   '.ttf': 'font/ttf'
